8cab918a155633f809835f39096402a97dad07f6
Luvuyo <bhekiluvuyo@gmail.com>
2024-07-01 22:55:19 +0200

    first commit

---
+++ tdd-using-mocha/test.greet.js
@@ -0,0 +1,11 @@
+describe("the greet function", function() {
+
+    it("should greet Rhyno with 'Hello,Rhyno'", function(){
+        assert.equal("Hello, Rhyno", greet("Rhyno"))
+});
+
+it("should greet Tsego with 'Hello,Tsego'", function(){
+assert.equal("Hello, Tsego", greet("Tsego"))
+});
+
+});
